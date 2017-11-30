fe61ecba31797e4a6d684f1339317a08ae087980
Dickon Reed <dickon.reed@citrix.com>
2017-11-30 13:55:24 +0000

    include StreamedActions.cs

---
+++ Lib/StreamedAction.cs
@@ -0,0 +1,27 @@
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System;
+using static Lib.StructuredLogger;
+
+namespace Lib {
+    public static class StreamedAction {
+        public static async Task Go(HttpResponse response, Func<Task> invoke) {            
+            // chunking inspired from https://stackoverflow.com/questions/42722936/asp-net-core-1-1-chunked-responses
+            response.StatusCode = 200;
+            response.ContentType = "text/html";
+            await response.WriteAsync($"<html><head><link rel=\"stylesheet\" href=\"https://ajax.aspnetcdn.com/ajax/bootstrap/3.3.7/css/bootstrap.min.css\" /> <link rel=\"stylesheet\" href=\"/css/site.css\" /></head> <body>");
+            // Edge, Firefox and IE will all wait 10+ seconds before showing anything if you send them a small amount of chunked HTML including a piece that will display. Chrome displays it immediately. If you send 1000 spaces in the first chunk they all do what I wanted. Ugh.
+            await response.WriteAsync(string.Concat(Enumerable.Repeat(" ", 1000)));
+            try {
+                SetupRecording(async x=> await response.WriteAsync(x));
+                await invoke.Invoke();
+                await response.WriteAsync("</body></html>");
+                await response.Body.FlushAsync();
+            } catch (Exception ex) {
+                await response.WriteAsync($"<strong>ERROR: <PRE>{ex.ToString()}</PRE>");
+            }
+        }
+    }
+}
